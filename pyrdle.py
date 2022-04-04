028a057ff913518a7be3f7aa2ba6bbc6298b017d
Thomas Loupe <6563450+thomasloupe@users.noreply.github.com>
2022-04-04 16:50:09 +0000

    Fix case where if the same letter was used more than once and was found twice in the chosen word, it would print twice. Fix not revealing word after losing. Changed some text statements and made them more friendly.

--- pyrdle.py
+++ pyrdle.py
@@ -17,39 +17,47 @@ def main_loop():
     global guesses
     # Check if we ran out of guesses first and act accordingly.
     if guesses >= 1:
-        print ("\nGuess the five letter word! You have", guesses, "guesses left...")
+        print ("\n", guesses, "tries left...")
     elif guesses == 0:
-        print ("\nThis is your last guess. Good luck!")
+        print ("\nThis is your last chance. Good luck!")
         # End the game when we run out of guesses.
     elif guesses < 0:
-        print ("You lose, the word was", chosen_word + ".")
+        print ("\nThe word was", chosen_word + ".")
+        quit()
     # Read the user's input
     prompt = input().strip().casefold()
     # If the length of the word isn't 5 characters, return. This also fixes the "pick every character cheat"
     if len(prompt) != 5:
-        print("That word isn't 5 characters!")
+        print("That word isn't 5 letters!")
         return
         # If the word isn't actually in the English dictionary, don't count this as a guess.
     if d.check(prompt) != True:
-        print ("That's not a word.")
+        print ("Not a valid word.")
         return
     # Set up a variable so we can see how many letters matched, if any.
-    matched_letters = 0
     if prompt.casefold().strip() == chosen_word.casefold().strip():
         print("YOU WIN!!!")
+        quit()
     else:
         # Enter a for loop that checks each letter the user chose against each letter in the chosen 
         # random word and see if there's any matches in letters.
         # Take away one guess.
         guesses -= 1
-        for letter in chosen_word:
-            for each_letter in prompt:
-                if each_letter == letter:
-                    print ("The letter " + letter + " is in the word!")
-                    matched_letters += 1
-                    if matched_letters == 0:
-                        print ("No matched letters found in your choice...")
+        characters_in_chosen_word = []
+        
+        for letter in prompt:
+            if letter in chosen_word:
+                if letter in characters_in_chosen_word:
+                    pass
+                else:
+                    characters_in_chosen_word.append(letter)
+        if len(characters_in_chosen_word) != 0:
+            for char in characters_in_chosen_word:
+                print ("The letter", char, "is in the word!")
+        else:
+            print("None of the letters are in the word.")
+                
 
 # Start the loop and only break when we're at negative guesses
-while guesses < 7 and guesses > -1:
+while guesses < 7 and guesses >= -1:
     main_loop()
