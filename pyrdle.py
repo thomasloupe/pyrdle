b696e529ab20aba80c89cd8885d40c285f669249
Thomas Loupe <6563450+thomasloupe@users.noreply.github.com>
2022-04-04 15:41:27 +0000

    Fix infinite loop. Move input to later section.

--- pyrdle.py
+++ pyrdle.py
@@ -15,40 +15,40 @@ chosen_word = random.choice(open("words_alpha.txt").readlines())
 def main_loop():
     # Access the global variable.
     global guesses
-    # Take away one guess.
     # Check if we ran out of guesses first and act accordingly.
     if guesses >= 1:
         print ("\nGuess the five letter word! You have", guesses, "guesses left...")
     elif guesses == 0:
         print ("\nThis is your last guess. Good luck!")
-        # Read the user's input
-        prompt = input().strip().casefold()
-        # If the length of the word isn't 5 characters, return. This also fixes the "pick every character cheat"
-        if len(prompt) != 5:
-            print("That word isn't 5 characters!")
-            return
-        # If the word isn't actually in the English dictionary, don't count this as a guess.
-        if d.check(prompt) != True:
-            print ("That's not a word.")
-            return
-        # Set up a variable so we can see how many letters matched, if any.
-        matched_letters = 0
-        if prompt.casefold().strip() == chosen_word.casefold().strip():
-            print("YOU WIN!!!")
-        else:
-            # Enter a for loop that checks each letter the user chose against each letter in the chosen 
-            # random word and see if there's any matches in letters
-            guesses -= 1
-            for letter in chosen_word:
-                for each_letter in prompt:
-                    if each_letter == letter:
-                        print ("The letter " + letter + " is in the word!")
-                        matched_letters += 1
-                        if matched_letters == 0:
-                            print ("No matched letters found in your choice...")
-    # End the game when we run out of guesses.
+        # End the game when we run out of guesses.
     elif guesses < 0:
         print ("You lose, the word was", chosen_word + ".")
+    # Read the user's input
+    prompt = input().strip().casefold()
+    # If the length of the word isn't 5 characters, return. This also fixes the "pick every character cheat"
+    if len(prompt) != 5:
+        print("That word isn't 5 characters!")
+        return
+        # If the word isn't actually in the English dictionary, don't count this as a guess.
+    if d.check(prompt) != True:
+        print ("That's not a word.")
+        return
+    # Set up a variable so we can see how many letters matched, if any.
+    matched_letters = 0
+    if prompt.casefold().strip() == chosen_word.casefold().strip():
+        print("YOU WIN!!!")
+    else:
+        # Enter a for loop that checks each letter the user chose against each letter in the chosen 
+        # random word and see if there's any matches in letters.
+        # Take away one guess.
+        guesses -= 1
+        for letter in chosen_word:
+            for each_letter in prompt:
+                if each_letter == letter:
+                    print ("The letter " + letter + " is in the word!")
+                    matched_letters += 1
+                    if matched_letters == 0:
+                        print ("No matched letters found in your choice...")
 
 # Start the loop and only break when we're at negative guesses
 while guesses < 7 and guesses > -1:
